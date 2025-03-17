f1b42f72cdb3c2e5e9d1b10f483b770059b5dcc2
hanjh1217 <hanjunghyen826@gmail.com>
2025-03-17 11:46:41 +0900

    [Feat] Excel DB 제작 후 Scene에서 로드 까지
    이후 스크립터블 오브젝트를 생성하여 관리

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+[Serializable]
+
+public class ItemData : MonoBehaviour
+{
+    public int id;
+    public string itemName;
+    public string description;
+    public string nameEng;
+    public string itemTypeString;
+    [NonSerialized]
+    public ItemType itemType;
+    public int price;
+    public int power;
+    public int level;
+    public bool isStackble;
+    public string iconPath;
+
+    //문자열을 열거형으로 변환하는 메서드
+
+    public void InitalizeEnums()
+    {
+        if (Enum.TryParse(itemTypeString, out ItemType parsedType))
+        {
+            itemType = parsedType;
+
+        }
+        else
+        {
+            Debug.LogError($"아이템 ' {itemName} '에 유효하지 않은 아이템 타입 : {itemTypeString}");
+            //기본값 설정
+            itemType = ItemType.Consumable;
+        }
+    }
+}
---
+++ Assets/Scripts/ItemType.cs
@@ -0,0 +1,8 @@
+public enum ItemType
+{
+    Wepon,
+    Armor,
+    Consumable,
+    Shield,
+    Accessory,
+}
