05a7f0d1b2ffad6ae72557390e9fd7552ea0f5bc
minkkjean <mkkjin@g.shingu.ac.kr>
2025-05-26 10:57:27 +0900

    [Feat] ToasatMessage & CheatSystem

--- Assets/CardGame/CardManager.cs
+++ Assets/CardGame/CardManager.cs
@@ -65,13 +65,15 @@ public class CardManager : MonoBehaviour
         if(handCards.Count >= 6)                //손패가 이미 6장 이상이면 드로우 하지 않음
         {
             Debug.Log("손패가 가득 찼습니다.!(최대6장)");
-            return;
+            ToastMessage.Instance.ShowMessage("손패가 가득 찼습니다. ! (최대6장) ", ToastMessage.MessageType.Warning);
+            return; 
 
         }
 
         if(deckCards.Count == 0)
         {
             Debug.Log("덱에 카드가 없습니다.");
+            ToastMessage.Instance.ShowMessage("덱에 카드가 없습니다.", ToastMessage.MessageType.Warning);
             return;
         }
 
@@ -99,6 +101,7 @@ public class CardManager : MonoBehaviour
         ArrangeHand();
 
         Debug.Log("카드를 드로우 했습니다. :" + carData.cardName + "(손패 : " + handCards.Count + "/6");
+        ToastMessage.Instance.ShowMessage("카드를 드로우 했습니다. : " + carData.cardName + " (손패 :" + handCards.Count +  "/6", ToastMessage.MessageType.info); 
     }
 
     public void ArrangeHand()           //손에 있는 카드 재정렬
